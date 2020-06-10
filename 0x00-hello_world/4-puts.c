8a04eac2e2a4b7009fe507f56f807128cdee83a1
camilo6 <trresqwe@gmail.com>
2020-06-10 19:18:59 +0000

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-
+/**
+ *main - print string to the console with a newline
+ *Return: string with return value of zero
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
