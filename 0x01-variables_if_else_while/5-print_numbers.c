91ebb5cff3c530066d8c553610f216c5a9921863
camilo6 <trresqwe@gmail.com>
2020-06-11 20:02:52 +0000

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Everything works here
+ *
+ * return - Must always come back on 0
+ */
+
+int main(void)
+{
+	int j = 0;
+
+	while  (j < 10)
+	{
+		pritnf(j);
+		j = j + 1;
+	}
+}
