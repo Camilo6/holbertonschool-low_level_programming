bd215dd955b41114c2822ea393c65d6958ecea3d
camilo6 <trresqwe@gmail.com>
2020-06-11 22:22:02 +0000

    print comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Everything works here
+ *
+ * Return: Must always come back on 0
+ **/
+int main(void)
+{
+	int n;
+
+	n = 48;
+
+	while (n <= 57) /*57 corresponds with ASCII character 9*/
+	{
+		putchar (n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		n++;
+	}
+	putchar ('\n'); /*add new line*/
+	return (0);
+}
