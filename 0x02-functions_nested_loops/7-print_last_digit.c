53d0989f9853778d9f7f131a3e470c83e0f9d51f
camilo6 <trresqwe@gmail.com>
2020-06-16 17:20:11 +0000

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * _abs - returns absolute value of parameter
+ *
+ * @n: int to turn to absolute value
+ * Return: returns absolute value
+ */
+int _abs(int n)
+{
+	if (n <= 0)
+		return (n - (n * 2));
+	else
+		return (n);
+}
+
+/**
+ * print_last_digit - prints the last digit of a number
+ *
+ * Returns: the value of the last digit
+ */
+
+int print_last_digit(int n)
+{
+	int last;
+
+	last = _abs(n);
+	last %= 10;
+
+	_putchar(last + '0');
+	return (last);
+}
