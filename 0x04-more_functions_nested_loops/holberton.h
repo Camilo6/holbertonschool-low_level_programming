aa2866514febaf84376500aff4e6e9673fb06280
camilo6 <trresqwe@gmail.com>
2020-06-18 17:09:22 +0000

    holberton head

---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,8 @@
+void print_line(int n);
+void more_numbers(void);
+void print_most_numbers(void);
+void print_numbers(void);
+int _putchar(char);
+int mul(int a, int b);
+int _isdigit(int c);
+int _isupper(int c);
