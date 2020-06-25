893f2dd64d55b56a1f9a6252a6b5bd415d711c73
camilo6 <trresqwe@gmail.com>
2020-06-25 18:36:04 +0000

    strign concatenetet 2

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * _strncat - concatenates two strings
+ * @dest: string
+ * @src: string
+ * @n: integer
+ * Return: string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int x = 0, y = 0;
+
+	while (*(dest + x) != '\0')
+	{
+		x++;
+	}
+	while (y < n && *(src + y) != '\0')
+	{
+	       dest[x + y] = src[y];
+		y++;
+	}
+	dest[x + y] = '\0';
+	return (dest);
+}
