2240aa0ea46e26a96174f0086a903c848d8533a7
camilo6 <trresqwe@gmail.com>
2020-06-25 21:50:47 +0000

    rev array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * reverse_array - reverses the content of an array of integers.
+ *
+ * @a : Pointer that is parameter in the function.
+ * @n : String 1
+ * Return: Always dest.
+ */
+ void reverse_array(int *a, int n)
+ {
+	 int rev;
+	 int k;
+
+	 for (rev = 0; rev < n; rev++)
+	 {
+		 k = a[rev];
+		 a[rev] = a[n - 1];
+		 a[--n] = k;
+	 }
+ }
