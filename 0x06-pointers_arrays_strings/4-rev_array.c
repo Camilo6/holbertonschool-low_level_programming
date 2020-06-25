782279af526c0406b2e84fb932e4d3cc38325513
camilo6 <trresqwe@gmail.com>
2020-06-25 21:52:47 +0000

    rev array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -7,15 +7,15 @@
  * @n : String 1
  * Return: Always dest.
  */
- void reverse_array(int *a, int n)
- {
-	 int rev;
-	 int k;
+void reverse_array(int *a, int n)
+{
+	int rev;
+	int k;
 
-	 for (rev = 0; rev < n; rev++)
-	 {
-		 k = a[rev];
-		 a[rev] = a[n - 1];
-		 a[--n] = k;
-	 }
- }
+	for (rev = 0; rev < n; rev++)
+	{
+		k = a[rev];
+		a[rev] = a[n - 1];
+		a[--n] = k;
+	}
+}
