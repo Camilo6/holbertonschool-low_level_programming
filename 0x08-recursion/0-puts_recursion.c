a6c4c925b75805083564a478cfdfa26a710f4015
camilo6 <trresqwe@gmail.com>
2020-07-01 19:15:26 +0000

    print string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * _puts_recursion -  prints a string
+ * @s: char string
+ * Return: 0
+ */
+
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s != '\0')
+	{
+		putchar(s[i]);
+		s++;
+		_puts_recursion(s);
+	}
+	else
+	{
+		putchar('\0');
+	}
+}
