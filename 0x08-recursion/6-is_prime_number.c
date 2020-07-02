eb6d46c08a1d3854a997558185bfdf3f314bf395
camilo6 <trresqwe@gmail.com>
2020-07-02 19:52:16 +0000

    prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,9 +1,10 @@
 #include "holberton.h"
 
 /**
- * is_prime_number - returns 1 if the input integer is a prime number, otherwise
- * return 0.
+ * is_prime_number - returns 1 if the input integer is a prime number,
+ * otherwise return 0.
  * @n: integer
+ * Return: 0 or 1
  */
 int is_prime_number(int n)
 {
@@ -15,26 +16,26 @@ int is_prime_number(int n)
 	{
 		return (1);
 	}
-	return (primo(2, n)) ;
+	return (primo(2, n));
 }
 
 /**
  * primo - return prime number
  * @x: integer
- * @y: integer
+ * @n: integer
  * Return: 0 or 1
  */
 
 int primo(int x, int n)
- {
-	 if ((n % x) == 0)
-	 {
-		 return (0);
-	 }
-	 if (x == n / 2)
-	 {
-		 return (1);
-	 }
-	 ++x;
-	 return (primo(x , n));
- }
+{
+	if ((n % x) == 0)
+	{
+		return (0);
+	}
+	if (x == n / 2)
+	{
+		return (1);
+	}
+	++x;
+	return (primo(x, n));
+}
