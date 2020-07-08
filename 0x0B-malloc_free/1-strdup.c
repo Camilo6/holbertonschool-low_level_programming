06485a826a2c4d7e74776cbedaeae701d0fa49b6
Camilo6 <trresqwe@gmail.com>
2020-07-08 15:28:57 +0000

    copy

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -18,7 +18,8 @@ char *_strdup(char *str)
 	if (str == NULL)
 		return (NULL);
 
-	for (i = 0; *(str + i) != '\0'; i++);
+	for (i = 0; *(str + i) != '\0'; i++)
+		;
 	s = malloc(i * sizeof(*s) + 1);
 	if (s == NULL)
 	{
