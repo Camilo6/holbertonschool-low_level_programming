301803655f28866bc4c8c06c56d7aec51c24a91b
Camilo6 <trresqwe@gmail.com>
2020-07-10 17:04:43 +0000

    holberton header

--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -1,6 +1,8 @@
 #ifndef HOLBERTON
 #define HOLBERTON
 
+int _strlen(char *s);
+char *str_concat(char *s1, char *s2);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
