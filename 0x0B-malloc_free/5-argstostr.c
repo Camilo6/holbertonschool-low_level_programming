8f391e75ddf5d9bfaf29d3efee97e5c6f3e59c3c
Camilo6 <trresqwe@gmail.com>
2020-07-10 17:08:38 +0000

    arg

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -10,40 +10,37 @@
  *The function should return NULL on failure
  */
 
-char *str_concat(char *s1, char *s2)
+int i, j, len = 0;
+int k = 0;
+char *str;
+
+if (ac == 0 || av == '\0')
+	return ('\0');
+
+for (i = 0; i < ac ; i++)
 {
-	char *a;
-	int i;
-	int j;
-	int c;
-	int d;
+	len += _strlen(av[i]);
+}
 
-	if (s1 == NULL)
-	{
-		s1 = "";
-	}
-	if (s2 == NULL)
-	{
-		s2 = "";
-	}
-	for (i = 0; s1[i] != '\0'; i++)
-		;
-	for (j = 0; s2[j] != '\0'; j++)
-		;
-	a = malloc((i * sizeof(*s1)) + (j * sizeof(*s2)) + 1);
+len = len + ac;
+str = malloc(sizeof(char) * len + 1);
 
-	if (a == NULL)
+if (str == '\0')
+{ return ('\0'); }
+
+for (i = 0; i < ac; i++)
+{
+	for (j = 0; j < _strlen(av[i]); j++)
 	{
-		return (NULL);
+		str[k] = av[i][j];
+		k++;
 	}
-	for (c = 0, d = 0; c < (i + j + 1); c++)
-	{
-		if (c < i)
-			a[c] = s1[c];
-		else
-			a[c] = s2[d++];
-	}
-	return (a);
+	str[k] = '\n';
+	k++;
+}
+
+str[k] = '\0';
+return (str);
 }
 
 /**
@@ -64,45 +61,4 @@ int _strlen(char *s)
 	}
 
 	return (c);
-}
-
-
-/**
- * argstostr - arguments to strings
- * Return: string
- * @ac: assf
- * @av: afsff
- */
-char *argstostr(int ac, char **av)
-{
-	char *p;
-	int sum = 0;
-	int i = 0;
-
-	if (ac == 0 || av == NULL)
-	{
-		return (NULL);
-	}
-
-	while (i < ac)
-	{
-		sum = sum + _strlen(av[i]);
-		i++;
-	}
-
-	p = (char *)malloc((sum * sizeof(char)) + ac + 1);
-
-	if (p == NULL)
-	{
-		return (NULL);
-	}
-	i = 0;
-	while (i < ac)
-	{
-		p = str_concat(p, av[i]);
-		i++;
-	}
-	return (p);
-
-
 }
