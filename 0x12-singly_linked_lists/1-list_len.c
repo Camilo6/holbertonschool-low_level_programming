52ddbc8aaed5515b38e8d1362ed8afa4c8b3e83e
Camilo6 <trresqwe@gmail.com>
2020-07-23 18:25:28 -0500

    list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,11 +1,21 @@
 #include "lists.h"
 
 /**
- * list_len -
- *
- * Return: 
+ * list_len - returns the number of elements in a linked list_t list.
+ * @h: string
+ * Return: number of elements linked list
  */
 size_t list_len(const list_t *h)
 {
+	size_t i = 0;
 
+	if (h == NULL)
+	{
+		return (i);
+	}
+	for (; h != NULL; i++)
+	{
+		h = h->next;
+	}
+	return (i);
 }
