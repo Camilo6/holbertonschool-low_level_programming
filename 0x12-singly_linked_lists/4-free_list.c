3a30c22f856c955681d2b1acb183e33ded37596f
Camilo6 <trresqwe@gmail.com>
2020-07-23 18:34:26 -0500

    free

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -2,7 +2,7 @@
 
 /**
  * free_list - free all space malloc'ed for list_t linked list
- * @head: pointer to the head of the list
+ * @head: pointer to the head 
  */
 void free_list(list_t *head)
 {
