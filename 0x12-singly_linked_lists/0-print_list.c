097a666019ed1b7bdb182be6885a4086b389a83d
Camilo6 <trresqwe@gmail.com>
2020-07-23 16:38:27 -0500

    linked lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,14 +1,15 @@
 #include "lists.h"
 
 /**
- * print_list -
- *
- * Return: 
+ * print_list - all the elements of a list_t list.
+ * @h: pointer string
+ * Return: number of nodes
  */
+
 size_t print_list(const list_t *h)
 {
 	int numnod;
-	
+
 	for (numnod = 0; h != NULL; numnod++)
 	{
 		if (h->str == NULL)
