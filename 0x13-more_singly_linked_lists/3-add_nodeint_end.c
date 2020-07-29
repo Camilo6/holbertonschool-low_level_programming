cc40415d8cbc71ce950222c4c55455ad177b8b35
Camilo6 <trresqwe@gmail.com>
2020-07-28 19:34:46 -0500

    add end

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,38 +1,29 @@
 #include "lists.h"
 
 /**
- * add_nodeint_end -
- *
- * Return: 
+ * add_nodeint_end - function that adds a new node at the end of a list
+ * @head: double pointer to head node
+ * @n: int value to store in new node
+ * Return: a new node
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	list_t *new_node;
-	list_t *current_node;
-	int i;
+	listint_t *new_node = NULL;
+	listint_t *current_node = NULL;
 
-	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
-		return (NULL);
-
-	if (*head != NULL)
+	for (current_node = *head; current_node && current_node->next != NULL; )
 	{
-		current_node = *head;
-		for (i = 0; current_node->next != NULL; i++)
-		{
-			current_node = current_node->next;
-		}
-		current_node->next = new_node;
+		current_node = current_node->next;
 	}
+	new_node = malloc(sizeof(listint_t)); /* allocate node */
+	if (new_node == NULL) /* check new node if empty */
+		return (NULL);
+	new_node->n = n;   /*put new data into new node */
+	new_node->next = NULL;  /*make next of new node end by pointing to NULL */
+
+	if (current_node)
+		current_node->next = new_node;
 	else
 		*head = new_node;
-
-	new_node->next = NULL;
-	new_node->str = strdup(str);
-	for (i = 0; str[i] != '\0'; i++)
-		;
-	new_node->len = i;
-
 	return (new_node);
-}
 }
