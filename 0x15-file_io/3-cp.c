20ed0888c2f6aa63abfe5365aacae609a703cbab
Camilo6 <trresqwe@gmail.com>
2020-08-05 14:24:45 -0500

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,48 +1,50 @@
 #include "holberton.h"
 /**
  * main - program that copies the content of a file to another file
- * @ac: Number of arguments
+ * @argc: Number of arguments
  * @av: Arguments
  * Return: 0 success
  */
-int main(int ac, char *av[])
+int main(int argc, char *av[])
 {
-	int file, file1, c;
-	ssize_t r, w;
-	char buffer[1024];
+	int fd, fd_to;
+	int check_in, check_out;
+	char buff[1024];
 
-	if (ac != 3)
+	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	}
-	file = open(av[1], O_RDONLY);
-	if (file == -1)
+	fd = open(av[1], O_RDONLY);
+	if (fd == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 	}
-	file1 = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (file1 == -1)
+	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", av[2]), exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 	}
-	r = w = 1;
-	while (r)
+	check_in = check_out = 1;
+	while (check_in)
 	{
-		r = read(file, buffer, 1024);
-		if (r == -1)
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
-		if (r > 0)
+		check_in = read(fd, buff, 1024);
+		if (check_in == -1)
+			dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", av[1]), exit(98);
+		if (check_in > 0)
 		{
-			w = write(file1, buffer, r);
-			if (w == -1)
-				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+			check_out = write(fd_to, buff, check_in);
+			if (check_out == -1)
+				dprintf(STDERR_FILENO,
+					"Error: Can't write to %s\n", av[2]), exit(99);
 		}
 	}
-	c = close(file);
-	if (c == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file), exit(100);
-	c = close(file1);
-	if (c == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file1), exit(100);
+	check_out = close(fd);
+	if (check_out == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd), exit(100);
+	check_out = close(fd_to);
+	if (check_out == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
 	return (0);
 }
