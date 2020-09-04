fab14b780fc98f1aa52c03b5b868cd49fd0a5783
Camilo6 <trresqwe@gmail.com>
2020-09-04 14:20:05 -0500

    3

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,9 +1,10 @@
 #include "lists.h"
 
 /**
- * add_dnodeint_end -
- *
- * Return: 
+ * add_dnodeint_end - adds a new node at the end of a dlistint_t list
+ * @head: double pointer to head of linked list
+ * @n: data to insert into linked list
+ * Return: the address of the new element, or NULL if it failed
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
